e14b02ddf0367307d1efde866f1bdf2e9f6c7aa4
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-22 21:47:48 +0100

    Add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import 'hammerjs';
 
-import { MaterialModule } from './material-module/material.module';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot([
+    ])
   ],
-  providers: [MaterialModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
