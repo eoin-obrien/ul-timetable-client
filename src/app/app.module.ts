d5da0b6e99fb0dcfa3e5a887cfbb549beea7eaa4
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-22 22:03:25 +0100

    Implement routing for homepage and timetable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 import 'hammerjs';
 
 import { MaterialModule } from './material/material.module';
+import { routing } from './routes';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    RouterModule.forRoot([
-    ])
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,11 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {TimetableComponent} from './timetable/timetable.component';
+
+const APP_ROUTES: Routes = [
+  {path: '', component: HomepageComponent, pathMatch: 'full'},
+  {path: ':id', component: TimetableComponent},
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
