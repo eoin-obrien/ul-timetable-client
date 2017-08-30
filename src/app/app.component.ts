7305526e6caf91183895b01b7a1fef13592c1f6c
Eoin O'Brien <eoinobrien910@gmail.com>
2017-08-30 21:46:56 +0100

    Refactor loading spinner to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MdIconRegistry} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
+import {Event as NavigationEvent, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,30 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  loading = true;
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private mdIconRegistry: MdIconRegistry) {
+  constructor(private router: Router,
+              private sanitizer: DomSanitizer,
+              private mdIconRegistry: MdIconRegistry) {
     mdIconRegistry.addSvgIcon('lecture', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/lecture.svg'));
     mdIconRegistry.addSvgIcon('tutorial', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/tutorial.svg'));
     mdIconRegistry.addSvgIcon('lab', sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/lab.svg'));
+    router.events.subscribe((navigationEvent: NavigationEvent) => {
+      this.navigationInterceptor(navigationEvent);
+    });
+  }
+
+  private navigationInterceptor(navigationEvent: NavigationEvent): void {
+    // Enter loading state when navigation starts
+    if (navigationEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    // Leave loading state when navigation ends
+    if (navigationEvent instanceof NavigationEnd
+      || navigationEvent instanceof NavigationCancel
+      || navigationEvent instanceof NavigationError) {
+      this.loading = false;
+    }
   }
 }
